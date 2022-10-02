036b6fe330c384ae5e24be03667de15b2c1e1064
KimberlyElicker <kimberlyelicker@gmail.com>
2022-10-02 17:16:19 -0400

    Edited tests to use import

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,23 +5,8 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 import unittest
 
-#from translator import english_to_french
-#from translator import french_to_english
-
-def english_to_french(english_text):
-    """Translates English text to French text"""
-    french_text = language_translator.translate(
-    text=english_text,
-    model_id='en-fr').get_result()
-    return french_text
-
-def french_to_english(french_text):
-    """Translates French text to English text"""
-    english_text = language_translator.translate(
-    text=french_text,
-    model_id='fr-en').get_result()
-    return english_text
-
+from translator import english_to_french
+from translator import french_to_english
 
 class TestMachineTranslation(unittest.TestCase):
     def test_english_to_french(self):
