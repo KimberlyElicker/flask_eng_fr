bd4552e40753296ce78e82621f1060c2a4a77466
KimberlyElicker <kimberlyelicker@gmail.com>
2022-10-02 17:03:02 -0400

    Added final block to tests

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,7 +1,22 @@
 import unittest
 
-from translator import english_to_french
-from translator import french_to_english
+#from translator import english_to_french
+#from translator import french_to_english
+
+def english_to_french(english_text):
+    """Translates English text to French text"""
+    french_text = language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+    return french_text
+
+def french_to_english(french_text):
+    """Translates French text to English text"""
+    english_text = language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    return english_text
+
 
 class TestMachineTranslation(unittest.TestCase):
     def test_english_to_french(self):
@@ -10,3 +25,6 @@ class TestMachineTranslation(unittest.TestCase):
     def test_french_to_english(self):
         self.assertNotEqual(french_to_english("Bonjour"), "Bonjour")
         self.assertEqual(french_to_english("Bonjour"), "Hello")
+
+if__name__=='__main__':
+    unittest.main()
