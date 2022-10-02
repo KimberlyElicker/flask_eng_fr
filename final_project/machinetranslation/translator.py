0b76e7d9aa441f98bb800bd604f2913b83a2f2ba
KimberlyElicker <kimberlyelicker@gmail.com>
2022-10-02 16:43:02 -0400

    Added methods

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,23 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+authenticator = IAMAuthenticator('{apikey}')
+language_translator = LanguageTranslatorV3(
+    version='{version}',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('{url}')
+
+def englishToFrench(englishText):
+    frenchText = language_translator.translate(
+    text=englishText,
+    model_id='en-fr').get_result()
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    englishText = language_translator.translate(
+    text=frenchText,
+    model_id='fr-en').get_result()
+    return englishText
